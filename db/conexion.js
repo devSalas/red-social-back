919b4c826c8aae75017619fd7992ef94678c6ee2
esteban salas <esalassulca@gmail.com>
2023-01-24 12:05:19 -0500

    factorizando codigo

--- db/conexion.js
+++ db/conexion.js
@@ -5,9 +5,8 @@ dotevn.config({path:"./.env"})
 
 export const connectDB = async ()=>{
   try{
-    const url = process.env.urlBD /* || "mongodb+srv://esteban:9mka6pw0IpMUW8hN@cluster0.sg2d9.mongodb.net/red-social?retryWrites=true&w=majority" */
-    
-    console.log(process.env.urlBD)
+    console.log(process.env.urlBD,8)
+    const url = process.env.urlBD
     await connect(url)
     
   }catch(err){
